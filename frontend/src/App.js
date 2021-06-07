8e6e6f09ea78f7530d6983bec19ca822c6ce9daf
Nirajan995 <nirajankunwor604@gmail.com>
2021-06-07 17:58:24 +0545

    Adding the shopping cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 import { Container } from "react-bootstrap";
 
@@ -15,6 +16,7 @@ const App = () => {
         <Container>
           <Route exact path="/" component={HomeScreen} />
           <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
